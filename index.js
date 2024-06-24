fbc324c15fc1b2f998452fb6bd8014bbc0eaba35
aliusa <aliusa9809@gmail.com>
2024-06-23 21:44:23 -0400

    Added express as a framework

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("No breaking errors. Website is online Alhamdulilah.")
+const express = require('express')
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('/life-x-deen', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log("Sent file.")
+});
+
+app.get('/css/home.css', (req, res) => {
+    res.sendFile(path.join(__dirname, "./css", "home.css"));
+    console.log("Sent file.")
+});
+
+app.get('/node_modules/hijrah-date/hijrah-date.js', (req, res) => {
+    res.sendFile(path.join(__dirname, "./node_modules/hijrah-date", "hijrah-date.js"))
+    console.log("Sent file.")
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port} Alhamdulilah.`);
+});
